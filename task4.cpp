73dcf46417be43a44cd246621856b52a4d471432
GalyaAngelova <g.angelovaa@abv.bg>
2016-11-09 21:42:24 +0200

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
     int num;
     bool flag = true;
-    cout << "Insert a number to check if it is prime: ";
+    cout << "Insert a number: ";
     cin >> num;
     for (int i = 2; i < num && flag; i++)
     {
         flag = num % i;
     }
-    cout << (flag ? "Yes" : "No");
-    system("pause");
+    cout << "Is it prime? " << (flag ? "Yes" : "No");
+
     return 0;
 }
